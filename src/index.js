f66b6dc8a11c61349c23ae3814c52f41444759d1
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-07-24 01:31:34 +0200

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv"
+import { connectDB } from "./db/index.js";
+
+connectDB()
+// import express from "express"
+// const app = express()
+
+
+// ;( async() =>{
+
+//   try{
+
+//   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//   app.on("error",(error)=>{
+
+//     console.log("ERROR:",error);
+//     throw error
+//   })
+//   app.listen(process.env.PORT, ()=>{
+
+//     console.log(`Àpp is listening on port ${process.env.PORT}`)
+//   })
+//   }
+//   catch(error){
+
+//     console.error("Error:",error)
+//   }
+// })()
